57e583f31f6d8db1c4ccf64da424c7de515eea9e
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-01-24 11:52:25 -0300

    first commit

---
+++ src/main/java/com/rafael/restapiattornatustest/RestApiAttornatusTestApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.restapiattornatustest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiAttornatusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiAttornatusTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafael/restapiattornatustest/RestApiAttornatusTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.restapiattornatustest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiAttornatusTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
