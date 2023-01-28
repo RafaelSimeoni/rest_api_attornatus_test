35cb2440f1111ed0bb268eb424d2257ab9155982
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-01-28 19:59:31 -0300

    feat: findPersonById endpoint created

--- src/main/java/com/rafael/restapiattornatustest/controllers/PersonController.java
+++ src/main/java/com/rafael/restapiattornatustest/controllers/PersonController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/person")
@@ -21,6 +20,11 @@ public class PersonController {
 
     @PostMapping
     public ResponseEntity<PersonDTO> savePerson(@RequestBody @Valid PersonForm personForm) {
-        return ResponseEntity.status(HttpStatus.OK).body(personService.savePerson(personForm));
+        return ResponseEntity.status(HttpStatus.CREATED).body(personService.savePerson(personForm));
     }
+
+   @GetMapping("/{personId}")
+   public ResponseEntity<PersonDTO> findPersonById(@PathVariable UUID personId) {
+        return ResponseEntity.status(HttpStatus.OK).body(personService.findPersonById(personId));
+   }
 }
--- src/main/java/com/rafael/restapiattornatustest/services/PersonService.java
+++ src/main/java/com/rafael/restapiattornatustest/services/PersonService.java
@@ -1,5 +1,6 @@
 package com.rafael.restapiattornatustest.services;
 
+import com.rafael.restapiattornatustest.exceptions.EntityNotFoundException;
 import com.rafael.restapiattornatustest.models.dtos.PersonDTO;
 import com.rafael.restapiattornatustest.models.entities.Person;
 import com.rafael.restapiattornatustest.models.forms.PersonForm;
@@ -8,6 +9,9 @@ import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PersonService {
@@ -23,5 +27,11 @@ public class PersonService {
         Person newPerson = modelMapper.map(personForm, Person.class);
         Person savedPerson = personRepository.save(newPerson);
         return modelMapper.map(savedPerson, PersonDTO.class);
+    }
+
+    public PersonDTO findPersonById(UUID id) {
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        if(optionalPerson.isEmpty()) throw new EntityNotFoundException("Person not found");
+        return modelMapper.map(optionalPerson.get(), PersonDTO.class);
     }
 }
