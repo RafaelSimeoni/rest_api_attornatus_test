a4dc481ad960a8b652625dfdc062b39d6d7fed30
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-01-29 21:44:11 -0300

    test: main method test

--- src/test/java/com/rafael/restapiattornatustest/RestApiAttornatusTestApplicationTests.java
+++ src/test/java/com/rafael/restapiattornatustest/RestApiAttornatusTestApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class RestApiAttornatusTestApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		RestApiAttornatusTestApplication.main(new String[] {});
 	}
 
 }
