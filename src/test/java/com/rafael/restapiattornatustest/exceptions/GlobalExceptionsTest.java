c8c9991c833519738160a445235af5aa389a2e38
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-01-30 13:47:19 -0300

    refactory: globalException test methods and attributes changed

--- src/test/java/com/rafael/restapiattornatustest/exceptions/GlobalExceptionsTest.java
+++ src/test/java/com/rafael/restapiattornatustest/exceptions/GlobalExceptionsTest.java
@@ -1,32 +1,42 @@
 package com.rafael.restapiattornatustest.exceptions;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 public class GlobalExceptionsTest {
 
+    @Mock
+    private HttpServletRequest httpServletRequest;
+    @Mock
+    private EntityNotFoundException entityNotFoundException;
+
+
     @InjectMocks
     private GlobalExceptions globalExceptions;
+
+
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+        startAttributes();
     }
 
     @Test
     void whenEntityNotFoundExceptionThenReturnNotFoundAndStandardErrorInstance() {
+
         ResponseEntity<StandardError>  response = globalExceptions.entityNotFoundHandlerMethod(
-                new EntityNotFoundException("Object not found"), new MockHttpServletRequest());
+                entityNotFoundException, httpServletRequest);
 
         assertNotNull(response);
         assertNotNull(response.getBody());
@@ -39,4 +49,9 @@ public class GlobalExceptionsTest {
         assertEquals("Not Found", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
     }
+
+    private void startAttributes() {
+        entityNotFoundException = new EntityNotFoundException("Object not found");
+    }
+
 }
